48e16dd44d0d954a3a0612e64630defa962617ee
Kristian Shafer <kristianshafer@gmail.com>
2025-01-23 18:36:23 -0600

    Added api pull to pokemon api

---
+++ src/lib/pokeapi.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   import { onMount } from 'svelte';
+   let curl: Promise<any>;
+    onMount(async () => {
+        
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151/");
+        curl = response.json();
+
+    });
+</script>
+
+{#await curl}
+ <div>One moment please...</div>
+{:then results}
+ <pre>{JSON.stringify(results, null, 2)}</pre>
+{:catch error}
+<div>{error.message}</div>
+{/await}
